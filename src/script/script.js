f99d05f39b13fd66b9ee509ed6a1f57ca695d496
mduong9586 <58188709+mduong9586@users.noreply.github.com>
2022-04-26 02:50:26 -0400

    Add files via upload

--- src/script/script.js
+++ src/script/script.js
@@ -1,1 +1,20 @@
+const milkChoices = Array.from(document.getElementsByName("milk-pref"));
+const caloriesChoices = Array.from(document.getElementsByName("calories"));
+const veggieChoices = Array.from(document.getElementsByName("protein"));
 
+const startBtn = document.querySelector("#btn-start");
+startBtn.addEventListener("click", getRecipe);
+
+function getRecipe() {
+    for(let i = 0; i < milkChoices.length; i++) {
+        for(let k = 0; k < caloriesChoices.length; k++){
+            for(let m = 0; m < veggieChoices.length; m++){
+                if(milkChoices[i].checked && caloriesChoices[i].checked && veggieChoices[i].checked) {
+                    location.href = "overallrecipe.html";
+                } else {
+                    break;
+                }
+            }
+        }
+    }
+}
